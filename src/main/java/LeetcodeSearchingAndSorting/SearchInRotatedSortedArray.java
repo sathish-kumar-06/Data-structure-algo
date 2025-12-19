d6e03b7e78e3527c888206c6c8dc24f5d20d6bb8
sathish-kumar-06 <sathishsjroman@gmail.com>
2025-12-19 08:40:20 +0530

    Rotated sorted array

---
+++ src/main/java/LeetcodeSearchingAndSorting/SearchInRotatedSortedArray.java
@@ -0,0 +1,33 @@
+package LeetcodeSearchingAndSorting;
+
+public class SearchInRotatedSortedArray {
+
+    public static boolean search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) return true;
+
+            if (nums[low] == nums[mid]) {
+                low++;
+                high --;
+                continue;
+            }
+
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid]) high = mid - 1;
+                else low = mid + 1;
+            } else {
+                if (nums[mid] <= target && target <= nums[high]) low = mid + 1;
+                else high = mid - 1;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 5, 6, 0, 0, 1, 2};
+        System.out.println(search(nums, 5));
+    }
+}
---
+++ src/main/java/LeetcodeSearchingAndSorting/SearchInsertPosition.java
@@ -0,0 +1,25 @@
+package LeetcodeSearchingAndSorting;
+
+public class SearchInsertPosition {
+    public static void main(String[] args) {
+        int[] arr = {1,3,4,5};
+        int target =2;
+        System.out.println(search(arr,target));
+    }
+
+    private static int search(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length-1;
+        while (start<=end){
+            int mid = start+(end-start)/2;
+            if(arr[mid] == target){
+                return mid;
+            } else if (arr[mid] > target) {
+                end = mid -1;
+            }else {
+                start = mid +1;
+            }
+        }
+        return start;
+    }
+}
